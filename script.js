c5be66630b86ba27afbd027fa9af44d553eaa46a
Parshanl <>
2023-05-13 12:48:45 -0700

    setting up table and clickables

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 // Open the Bootstrap modal
-$('#myModal').modal('show');
-
-// Close the Bootstrap modal
-$('#myModal').modal('hide');
-
-
-// Add a click event listener to a button with the ID 'myButton'
-document.getElementById('myButton').addEventListener('click', function() {
-    // Open the Bootstrap modal
-    $('#myModal').modal('show');
-  });
-  
