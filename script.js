5ee6fc449a2f1dd2e929b1570364786041ce8733
Parshanl <>
2023-05-13 11:32:12 -0700

    file set up

---
+++ script.js
@@ -0,0 +1,13 @@
+// Open the Bootstrap modal
+$('#myModal').modal('show');
+
+// Close the Bootstrap modal
+$('#myModal').modal('hide');
+
+
+// Add a click event listener to a button with the ID 'myButton'
+document.getElementById('myButton').addEventListener('click', function() {
+    // Open the Bootstrap modal
+    $('#myModal').modal('show');
+  });
+  
